335cae6aefc8f2d0293789362da3fa7b77dcddb0
Marin37 <57327931+Marin37@users.noreply.github.com>
2020-08-03 16:23:09 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express  = require('express');
+const mongoose = require('mongoose');
+const port = process.env.PORT     || 3000;
+const db = process.env.MONGODBURI || 'mongodb://localhost/hellodb';
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+const router = require('./routes/index');
+app.use('/', router);
+
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log(`DB connected @ ${db}`);
+  })
+  .catch(err => console.error(`Connection error ${err}`));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+app.use((req, res, next) => {
+  
+  
+});
