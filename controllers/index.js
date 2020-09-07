9ae2958b842c99379d9766c846c085078334c99f
Marin37 <57327931+Marin37@users.noreply.github.com>
2020-09-07 14:46:18 -0300

    terminar

--- controllers/index.js
+++ controllers/index.js
@@ -1,30 +1,29 @@
 const User = require('../models/User');
 
-exports.home = (req, res) => {
-  res.render('index');
-};
-
-exports.search = (req, res) => {
-  res.send('Implementar SEARCH');
-};
-
-/*
-
-
-const User = require('../models/User');
 // controllers/index.js
 exports.home = (req, res) => {
   User.find().sort('id').exec((err, users) => {
     for (let user of users) {
       user.age = Math.trunc((new Date() - user.birthday) / 31536000000);
     }
+    console.log(users);
     res.render('index', { users: users });
   });
 };
 
 exports.search = (req, res) => {
-  res.send('Implementar SEARCH');
+  let result = null;
+  User.findOne({ id: req.query.id }).exec((err, user) => {
+    if (user != null) {
+      user.age = Math.trunc((new Date() - user.birthday) / 31536000000);
+      result = [user];
+    }
+    res.render('index', { users: result });
+  })
 };
 
 
-*/
+
+
+
+
